72c5727c35cadfbaa2001aa10296e201a9e13168
Morpheus-23 <skyelah@icloud.com>
2021-12-19 02:09:06 +0000

    Added board render and some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,18 @@ function displayTime(second) {
 function endCount() {
   timeH.innerHTML = "Sorry, time is up!";
 }
+
+//=== game board functions =================================
+
+function renderBoard(size) {
+  for (var i = 0; i < size; i++) {
+      var gameBoard = document.getElementById("game-board");
+      //<div id="card1" class="card" onclick="cardClicked(event)"></div>
+      var card = document.createElement("div");
+      card.setAttribute("id", "card"+i);
+      card.setAttribute("class", "card");
+      card.setAttribute("onclick", "cardClicked(event)");
+      gameBoard.appendChild(card);
+  }
+}
+
